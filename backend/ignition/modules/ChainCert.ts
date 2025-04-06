2cc79412a509871f6bb92b0fc1526ef7c7152fcc
neo07 <neo07-proton@protonmail.com>
2025-04-06 14:37:04 +0200

    fix(back ignition): add IPFS data

--- backend/ignition/modules/ChainCert.ts
+++ backend/ignition/modules/ChainCert.ts
@@ -4,17 +4,20 @@ const ChainCertModule = buildModule("ChainCertModule", (m) => {
   const chainCert = m.contract("ChainCert");
 
   const owner = m.getAccount(0);
+  const ipfsData = [
+    "bafkreigxbqoq2gbym5r2kaiebdfjwdtz4jwi3lvxhggtg37ybb6c6qxqza",
+    "bafkreiezwxzlznbok66crtsp3ewfz4asbafmoer6zcublpbd7ncdcb2nkq",
+    "bafkreignds3b3mu6fxe6tyjz6zk4c7s3hxukarrttizfv3mizcbpqzpbvq"]
 
   for (let i = 0; i < 3; i++) {
     const serialNumber = `SN-${Date.now()}-${i}`;
     const publicId = `PID-${Date.now()}-${i}`;
-    
+
     const uniqueId = `mintProductDate${Date.now()}Count${i}`;
-    const metadataURI = 'bafkreicqrutbejnu273znextky4xltyyy35jxwyiuwo2i57nih25cn7pgi';
 
     console.log(`Serial Number: ${serialNumber}, Public ID: ${publicId}`);
 
-    m.call(chainCert, "mintProduct", [owner, metadataURI, serialNumber, publicId], {
+    m.call(chainCert, "mintProduct", [owner, ipfsData[i], serialNumber, publicId], {
       from: owner,
       id: uniqueId,
     });
