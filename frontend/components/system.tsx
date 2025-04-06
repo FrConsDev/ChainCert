7faf9b6fb44cadd19d8107c107ddcf42293152ca
neo07 <neo07-proton@protonmail.com>
2025-04-06 04:32:38 +0200

    refactor(front components): remove comment

--- frontend/components/system.tsx
+++ frontend/components/system.tsx
@@ -4,16 +4,9 @@ import ProductDetailsComponent from './product-details-component';
 const ChainCertSystem = () => {
   return (
     <>
-        <div className="flex flex-col items-center space-y-5 p-5">
-    <ProductDetailsComponent />
-    {/* Fonctionnalités bonus:
-    <div>    Transfer ownership
-    </div>
-    <div>    Report an incident
-    </div>
-    <div>    Care instruction
-    </div> */}
-    </div>
+      <div className="flex flex-col items-center space-y-5 p-5">
+        <ProductDetailsComponent />
+      </div>
     </>
   )
 }
