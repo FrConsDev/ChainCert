76f006be6b4e891a211107936a376d3d6085dea0
neo07 <neo07-proton@protonmail.com>
2025-04-06 04:29:29 +0200

    fix(front app): activate auth check

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -11,8 +11,7 @@ export default function Home() {
       {isConnected ? (
         <ChainCertSystem />
       ) : (
-        <ChainCertSystem />
-        // <NotConnected />
+        <NotConnected />
       )}
     </>
   );
